e55b68d54bf217e9449b373bb9b53b1dea34e2e5
Erebus <linculn.ab@gmail.com>
2024-09-10 00:41:31 -0700

    Fixed an identation error in the else: part of the get_card function

--- GO FISH recreated.py
+++ GO FISH recreated.py
@@ -1,4 +1,4 @@
-import random 
+import random
 suites = ["Spades", "Hearts", "Diamonds", "Clubs"]
 values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "King", "Queen"]
 deck = [(card, category) for category in suites for card in values]
@@ -6,22 +6,24 @@ deck_count = 48
 player_hand = []
 bot_hand = []
 
+
 def get_card(amount, person):
     global deck_count
     if person == "player":
         for i in range(amount):
             player_hand.append(deck[random.randint(0,len(deck)-1)])
             deck_count -= 1
-        else:
-            for i in range(amount):
-                bot_hand.append(deck[random.randint(0,len(deck)-1)])
-                deck_count -= 1
+    else:
+        for i in range(amount):
+            bot_hand.append(deck[random.randint(0,len(deck)-1)])
+            deck_count -= 1
+
 
 def ask_for_card(card, person):
     success = False
     if person == "player":
         if card in bot_hand:
-            for cards in bot_hand: 
+            for cards in bot_hand:
                 if card == cards[0]:
                     player_hand.append(card)
                     bot_hand.remove(card)
@@ -30,7 +32,7 @@ def ask_for_card(card, person):
             print("Bot does not have {asked_card}".format(asked_card = card))
             print("Drawing a card for you")
             get_card(1, "player")
-            
+           
     else:
         if card in player_hand:
             for cards in player_hand:
@@ -41,9 +43,10 @@ def ask_for_card(card, person):
             print("Player does not have {asked_card}".format(asked_card = card))
             print("Drawing a card for you")
             get_card(1, "bot")
-            
+           
 
-        
+
+       
 hand = []
 def cards_in_hand(person):
     global hand
@@ -55,16 +58,20 @@ def cards_in_hand(person):
         for card in bot_hand:
             hand.append(card[0])
 
+
     return hand
 
 
-        
+
+
+       
 turn_player = ""
 random_number = random.randint(0,1)
 if random_number == 0:
     turn_player = "player"
 else:
     turn_player = "bot"
+
 
 get_card(7, "player")
 get_card(7, "bot")
@@ -87,6 +94,7 @@ else:
         asking_card = input("Pick another card: ")
     ask_for_card(asking_card, "player")
 
+
 print(player_hand)
 print(bot_hand)
-    
+
